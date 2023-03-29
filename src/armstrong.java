a7892227f81b615bde5729019a0b7b6776ddbbe1
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-03-29 09:52:58 +0530

    armstrong number

---
+++ src/armstrong.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class armstrong {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int t=n;
+        int s=n;
+        int r=0;
+        int c=0;
+        while(n>0){
+            c++;
+            n=n/10;
+        }
+        while(s>0){
+            int temp=s%10;
+             r=(int)Math.pow(temp,c)+r;
+            s=s/10;
+        }
+        if(t==r){
+            System.out.println("the number is armstrong");
+        }
+        else{
+            System.out.println("the number is not armstrong");
+        }
+    }
+}
