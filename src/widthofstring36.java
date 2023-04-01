737d8545dbf2f2df1d367ea467644ebaa879a86b
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-04-01 13:50:41 +0530

    check end with is not not

---
+++ src/widthofstring36.java
@@ -0,0 +1,7 @@
+public class widthofstring36 {
+    public static void main(String[] args) {
+        String str="this is very beautiful";
+        System.out.println(str.endsWith("r"));
+        System.out.println(str.endsWith("beautiful"));
+    }
+}
