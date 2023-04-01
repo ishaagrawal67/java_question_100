685752f578a439f54eec8863e738b104ce928a49
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-04-01 13:52:55 +0530

    replace with another

---
+++ src/replacewithanother38.java
@@ -0,0 +1,6 @@
+public class replacewithanother38 {
+    public static void main(String[] args) {
+        String str="this are my beautiful girl";
+        System.out.println(str.replaceAll("are","is"));
+    }
+}
