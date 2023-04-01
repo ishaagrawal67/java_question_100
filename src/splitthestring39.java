c12dc81598deff91e0cd9affad403f9b84f297b0
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-04-01 13:52:17 +0530

    split string

---
+++ src/splitthestring39.java
@@ -0,0 +1,9 @@
+public class splitthestring39 {
+    public static void main(String[] args) {
+        String s1 = "java string split method by Technolamror";
+        String[] words = s1.split(" ");
+        for (String w : words) {
+            System.out.println(w);
+        }
+    }
+}
