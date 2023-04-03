b611105c83f677dd6325cb163c66a87b2cda0f1b
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-04-03 11:45:50 +0530

    convert int into string

---
+++ src/convertintintostring81.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class convertintintostring81 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        System.out.println(s+"");
+        System.out.println(String.valueOf(s));
+    }
+}
