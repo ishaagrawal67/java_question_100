eb2d8c9e3daaf609b55da48d817be62858a7bce1
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-04-03 11:24:59 +0530

    remove wide space

---
+++ src/removewhiteasape40.java
@@ -0,0 +1,7 @@
+public class removewhiteasape40 {
+    public static void main(String[] args) {
+        String s1=" hello string ";
+        System.out.println(s1+"Technolamror");//without trim()
+        System.out.println(s1.trim()+"Technolamror");//with trim()
+    }
+}
