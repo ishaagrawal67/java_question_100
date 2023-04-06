3eadd64440176f8e6c942f426c3a13114e8dc452
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-04-06 11:37:05 +0530

    convert string to float

---
+++ src/convertstringintodouble84.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class convertstringintodouble84 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String s=sc.next();
+       Double l=Double.parseDouble(s);
+        System.out.println(((Object)l).getClass().getSimpleName());
+    }
+}
