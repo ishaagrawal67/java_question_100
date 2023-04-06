ab423dd0931aeabf7741b978de085b53639cdcda
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-04-06 11:37:54 +0530

    convert string to long

---
+++ src/convertstringtolong82.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class convertstringtolong82 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        String n=sc.next();
+        String s = "44";
+        System.out.println(Long.parseLong(s));
+
+    }
+
+}
