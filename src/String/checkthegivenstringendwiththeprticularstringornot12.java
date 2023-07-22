a9b5c011126b1b6d46e69e0bce7b5a3114ae6abc
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-07-22 21:39:20 +0530

    check the given string end with the prticulr string

---
+++ src/String/checkthegivenstringendwiththeprticularstringornot12.java
@@ -0,0 +1,14 @@
+package String;
+
+public class checkthegivenstringendwiththeprticularstringornot12 {
+    public static void main(String[] args) {
+        String s="isha";
+        String t="agrawal";
+        String l="al";
+        boolean b=s.endsWith(l);
+        boolean bl=t.endsWith(l);
+        System.out.println("ends with the prticular string or not "+b);
+        System.out.println("ends with the prticular string or not "+bl);
+
+    }
+}
