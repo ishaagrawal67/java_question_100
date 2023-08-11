a32f40037d3d26f8cda2cf67d1043407a043caad
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 11:05:59 +0530

    first larger second min immure range in this way50

---
+++ src/String/getacharacterwithgivenindex01.java
@@ -0,0 +1,11 @@
+package String;
+
+public class getacharacterwithgivenindex01 {
+    public static void main(String[] args) {
+            String str="java exercise";
+            int index=str.charAt(0);
+            int index1=str.charAt(5);
+        System.out.println("the charcter st postion 0 is "+(char)index);
+        System.out.println("the chracter st position at 5 is "+(char)index1);
+    }
+}
