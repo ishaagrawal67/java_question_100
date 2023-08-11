9d7dd4176ddebd5bb1ffb2cb3c781d1792d0d6d5
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 11:15:59 +0530

    two string contains same data

---
+++ src/String/twostringcontainsamedata13.java
@@ -0,0 +1,10 @@
+package String;
+
+public class twostringcontainsamedata13 {
+    public static void main(String[] args) {
+        String str="isha";
+        String str1="agrawal";
+        boolean b=str.equals(str1);
+        System.out.println(b);
+    }
+}
