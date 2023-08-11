4ece1f19bf7fa9cee0a72908505447fc3381c22c
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 11:09:24 +0530

    print sum which is closest to zero41

---
+++ src/array/printsumwhichisclosesttozero41.java
@@ -0,0 +1,50 @@
+package array;
+
+public class printsumwhichisclosesttozero41 {
+    public static void main(String[] args) {
+//        int[] a={1, 5, -4, 7, 8, -6};
+//        int n=a.length;
+//        int sum=a[0]+a[1];
+//        int sm=0;
+//        int k=0;
+//        int l=0;
+//        for(int i=0;i<n;i++){
+//            for(int j=i+1;j<n;j++) {
+//                sm = a[i] + a[j];
+//                if (Math.abs(sm) < Math.abs(sum)) {
+//                    sum=sm;
+//                    k = i;
+//                    l = j;
+//                }
+//            }
+//        }
+//        System.out.println("two elements are "+a[k]+" "+a[l]);
+
+//        int k=20;
+//        if(k){
+//            System.out.println("yes");
+//        }
+//        else{
+//            System.out.println("no");
+//        }
+
+//     char ch='A';
+//     if(ch>70){
+//         System.out.println("pizza");
+//     }
+//     else{
+//         System.out.println("burger");
+//     }
+//         byte a=1;
+//         if(!a){
+//            System.out.println("fish");
+//        }
+//        else{
+//            System.out.println("jk");
+//        }
+
+
+
+
+    }
+}
