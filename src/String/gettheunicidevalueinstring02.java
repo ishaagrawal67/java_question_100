808e9f046a5a399657c9ff86acf574c78ea4b09d
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 11:06:40 +0530

    get the unicide value in string02

---
+++ src/String/gettheunicidevalueinstring02.java
@@ -0,0 +1,11 @@
+package String;
+
+public class gettheunicidevalueinstring02 {
+    public static void main(String[] args) {
+        String str="w3resources.com";
+        int val=str.codePointAt(1);
+        int val2=str.codePointAt(9);
+        System.out.println(val);
+        System.out.println(val2);
+    }
+}
