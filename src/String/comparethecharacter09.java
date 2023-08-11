1bf34cee99da8169a9aafde6e1ee9f5de65a8014
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 10:57:28 +0530

    compare the character09

---
+++ src/String/comparethecharacter09.java
@@ -0,0 +1,11 @@
+package String;
+
+public class comparethecharacter09 {
+    public static void main(String[] args) {
+        String s="isha";
+        String s1="Isha";
+        String s3="isha";
+        System.out.println("comapree two string is true or false "+s3.contentEquals(s));
+        System.out.println("comapree two string is true or false "+s3.contentEquals(s1));
+    }
+}
