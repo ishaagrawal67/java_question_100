f6ae6934d5dd219343fa4e4b7c8735acb245b293
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 10:59:06 +0530

    conctaenatethestrings07

---
+++ src/String/conctaenatethestrings07.java
@@ -0,0 +1,11 @@
+package String;
+
+public class conctaenatethestrings07 {
+    public static void main(String[] args) {
+        String str="isha";
+        String str1="agrawal";
+        //System.out.println("concatenate te string is "+ (str+" and "+str1));
+         String str3=str.concat(str1);
+        System.out.println("the string is "+ str3);
+    }
+}
