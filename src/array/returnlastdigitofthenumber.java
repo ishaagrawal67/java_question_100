61595712183e106605906229a4e9c09dde176427
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 11:10:59 +0530

    return last digit of the number

---
+++ src/array/returnlastdigitofthenumber.java
@@ -0,0 +1,30 @@
+//package array;
+//
+//import org.w3c.dom.ls.LSOutput;
+//
+//public class returnlastdigitofthenumber {
+//
+//    public static void main(String[] args) {
+//        int r=3456;
+//        int n=2;
+//        System.out.println(r%10);
+//        int t=0;
+//        while(n>0){
+//            t=0;
+//            t=r%10;
+//            n--;
+//            r=r/10;
+//        }
+//        System.out.println(t);
+//
+//    byte a=0b0000_0001;
+//    System.out.println(~a);
+//
+//    int b=45;
+//    String str="";
+////    while(b>0){
+////        str=str+b%
+////
+////
+////    }
+//}
