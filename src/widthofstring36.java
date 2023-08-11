ec6cc4cf06d73343a016531ff61aa51c0ea560a0
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 11:16:17 +0530

    width of string

--- src/widthofstring36.java
+++ src/widthofstring36.java
@@ -1,7 +1,13 @@
 public class widthofstring36 {
     public static void main(String[] args) {
-        String str="this is very beautiful";
-        System.out.println(str.endsWith("r"));
-        System.out.println(str.endsWith("beautiful"));
+//        String str="this is very beautiful";
+//        System.out.println(str.endsWith("r"));
+//        System.out.println(str.endsWith("beautiful"));
+        int number = 2;
+
+        // 2 bit left shift operation
+        int Ans = number << 2;
+
+        System.out.println(Ans);
     }
 }
