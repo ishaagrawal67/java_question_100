62daece39bad80a68d4430313787846f8de28528
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 10:59:25 +0530

    counttheindexinagivenrange04

---
+++ src/String/counttheindexinagivenrange04.java
@@ -0,0 +1,9 @@
+package String;
+
+public class counttheindexinagivenrange04 {
+    public static void main(String[] args) {
+        String str="java exercise";
+        int vl=str.codePointCount(1,10);
+        System.out.println(vl);
+    }
+}
