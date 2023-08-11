4512d034f0a38c181c02a4f256e9f1ec3ebe9801
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-11 11:07:02 +0530

    char at

---
+++ src/String/i1.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.Scanner;
+
+public class i1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        int i=0,j=7;
+        System.out.println("character at position "+i+" is "+str.charAt(i));
+        System.out.println("character at position "+j+" is "+str.charAt(j));
+    }
+}
