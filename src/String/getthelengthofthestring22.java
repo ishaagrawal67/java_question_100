a632dde888352f6af35b9f8cff7aa49816fe51eb
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-12 15:18:15 +0530

    length of the string

---
+++ src/String/getthelengthofthestring22.java
@@ -0,0 +1,8 @@
+package String;
+
+public class getthelengthofthestring22 {
+    public static void main(String[] args) {
+        String str="Isha Agrawal";
+        System.out.println(str.length());
+    }
+}
