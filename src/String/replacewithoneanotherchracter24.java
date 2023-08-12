07d0288acff2ff7c8edb2399098ff486cbc301d0
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-12 15:18:42 +0530

    replace with one another character

---
+++ src/String/replacewithoneanotherchracter24.java
@@ -0,0 +1,9 @@
+package String;
+
+public class replacewithoneanotherchracter24 {
+    public static void main(String[] args) {
+        String st="Isha Agdawal";
+        String s=st.replace('d','r');
+        System.out.println(s);
+    }
+}
