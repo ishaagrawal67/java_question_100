e53850d33fc425790901a66bbde75ad5a0834d6a
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-12 15:17:52 +0530

    get the the index os the alphabet character in the string index

---
+++ src/String/gettheindexofthealphabetchr19.java
@@ -0,0 +1,17 @@
+package String;
+
+public class gettheindexofthealphabetchr19 {
+    public static void main(String[] args) {
+        String str="Isha Agrawal";
+        int a=str.indexOf("a");
+        int b=str.indexOf("s");
+        int c=str.indexOf("h");
+        int d=str.indexOf("I");
+        int e=str.indexOf("l");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
