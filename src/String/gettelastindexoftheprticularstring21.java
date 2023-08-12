d95036c125e073e5cd3a562ffcbf326a25834ebb
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-12 15:17:13 +0530

    get last index of the prticulae given index

---
+++ src/String/gettelastindexoftheprticularstring21.java
@@ -0,0 +1,15 @@
+package String;
+
+public class gettelastindexoftheprticularstring21 {
+    public static void main(String[] args) {
+        String str="Isha Agrawal";
+        int a=str.lastIndexOf("a",str.length()-1);
+        int b=str.lastIndexOf("l",str.length()-1);
+        int c=str.lastIndexOf("w",str.length()-1);
+        int d=str.lastIndexOf("r",str.length()-1);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
