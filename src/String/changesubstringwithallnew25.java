0a5d544e8cbf4cb18d9b55a20d70ae10fd9d8203
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-12 15:15:17 +0530

    chnange substring with anothe string

---
+++ src/String/changesubstringwithallnew25.java
@@ -0,0 +1,9 @@
+package String;
+
+public class changesubstringwithallnew25 {
+    public static void main(String[] args) {
+        String str="Isha is a bad girl all bad girl is beautiful and honest";
+        String n=str.replaceAll("bad","good");
+        System.out.println(n);
+    }
+}
