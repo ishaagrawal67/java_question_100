ae2f85c1c72098b16d5b898e2c03050e4824ab17
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2023-08-12 15:15:59 +0530

    chaeck string is strt with the string or not

---
+++ src/String/checkthestringisstartiwiththtstringornot26.java
@@ -0,0 +1,11 @@
+package String;
+
+public class checkthestringisstartiwiththtstringornot26 {
+    public static void main(String[] args) {
+        String str="isha agrawal";
+        String name="isha";
+        String na="Isha";
+        System.out.println(str.startsWith(name));
+        System.out.println(str.startsWith(na));
+    }
+}
